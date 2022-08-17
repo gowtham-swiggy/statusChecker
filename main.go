986ad31bbe30f0ab873b4c734f4239e2165017dc
Indukuri Gowtham Kishore <indukuri.kishore@MacBook-Pro.local>
2022-08-17 10:21:50 +0530

    updated main.go file with API's

--- main.go
+++ main.go
@@ -1,6 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 
 func main() {
-	
+	fmt.Println("Hello Netizen!...")
+	// Home Page API
+
+	// Post API
+
+	//Get Details API
+
+	//Get Single Details API
+
+	//Anything API
+
+	http.ListenAndServe(":5000",nil)
+
+}
+
+func homePageHandler(w http.ResponseWriter,r * http.Request) {
+	fmt.Fprint(w,"Error Home...")
+	fmt.Println("You are in anything handler")
+}
+
+func postHandler(w http.ResponseWriter,r * http.Request) {
+	fmt.Fprint(w,"Error 4Post...")
+	fmt.Println("You are in anything handler")
+}
+
+func getHandler(w http.ResponseWriter,r * http.Request) {
+	fmt.Fprint(w,"Error Get...")
+	fmt.Println("You are in anything handler")
+}
+
+func getSingleHandler(w http.ResponseWriter,r * http.Request) {
+	fmt.Fprint(w,"Error Gets ..")
+	fmt.Println("You are in anything handler")
+}
+
+func anythingHandler(w http.ResponseWriter,r * http.Request) {
+	fmt.Fprint(w,"Error 404...")
+	fmt.Println("You are in anything handler")
 }
