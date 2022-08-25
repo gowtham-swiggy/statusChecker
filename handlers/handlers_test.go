1108a02616b021d6e2e20a59e244c732f8545f81
Indukuri Gowtham Kishore <indukuri.kishore@MacBook-Pro.local>
2022-08-25 10:09:56 +0530

    updated the unit test cases

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,23 +1,86 @@
 package handlers
 
-import "testing"
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-func TestHomePageHandler(t*testing.T) {
-
+func TestHomePageHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/home", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(HomePageHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := "Welcome to Home Page"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 }
 
-func TestAnythingHandler(t*testing.T) {
-	
+func TestPostHandler(t *testing.T) {
+	var jsonData = []byte(`{"website":["www.github.com/igkishore01/statuschecker"]}`)
+	req, err := http.NewRequest("POST", "/post", bytes.NewBuffer(jsonData))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(PostHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := "map[www.github.com/igkishore01/statuschecker:404]"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 }
 
-func TestPostHandler(t*testing.T) {
-	
+func TestGetHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/get", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := "The status of website www.github.com/igkishore01/statuschecker is DOWN"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 }
 
-func TestGetHandler(t*testing.T) {
-	
-}
-
-func TestGetSingleHandler(t*testing.T) {
-	
+func TestGetSingleHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/getsingle?name=www.github.com/igkishore01/statuschecker", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetSingleHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := "The status of website www.github.com/igkishore01/statuschecker is DOWN"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 }
